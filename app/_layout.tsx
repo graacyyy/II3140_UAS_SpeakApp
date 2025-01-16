c06e5bbca590c009a6ed899e67cb28ce5acd2190
rhleena <118552028+rhleena@users.noreply.github.com>
2025-01-16 19:02:02 +0700

    quiz update + quiz result

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,16 @@ import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen 
+        name="(quiz)/quizscreen" 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="(quiz)/quizresults" 
+        options={{ headerShown: false }} 
+      />
+    </Stack>
   );
 }
